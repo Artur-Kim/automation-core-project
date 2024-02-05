b075c3c4d14a64cb7b99324697d560f0bcc3867c
U_M1FNH <artursergeevich93@gmail.com>
2024-02-05 21:30:52 +0300

    Homework - 2

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -12,7 +12,7 @@ public class SearchTests {
     static void beforeCondition() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = false;
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
